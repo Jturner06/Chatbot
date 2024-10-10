5d4604872504a8ae4e7fef7ceb037030f9897ec9
Jturner06 <jrturner1220@gmail.com>
2024-10-10 18:27:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
 
+# Create a new chatbot instance
+chatbot = ChatBot(
+    'MyChatBot',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    logic_adapters=[
+        'chatterbot.logic.BestMatch'
+    ],
+    database_uri='sqlite:///database.sqlite3'
+)
